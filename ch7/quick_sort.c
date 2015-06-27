f8ef02d9b31d2b3d0a90fc5035cfde0ec1223c7d
chenjinxian <alanchen1203@gmail.com>
2015-06-27 10:16:54 +0800

    add quick sort

---
+++ ch7/quick_sort.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_NUM 12
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int array[], int start, int end)
+{
+    int pivot_element = array[end];
+    int i = start;
+
+    for (int j = start; j < end; j++) {
+        if (array[j] < pivot_element) {
+            swap(&array[i], &array[j]);
+            i++;
+        }
+        //for solution 7.1-2
+        //if all element in array are the same, only add i once in every two compare
+        else if (array[j] == pivot_element) {
+            if (j % 2)
+                i++;
+        }
+    }
+
+    swap(&array[i], &array[end]);
+
+    printf("\n");
+    for (int j = start; j <= end; j++) {
+        printf("%d\t", array[j]);
+    }
+    printf("\n");
+    
+    return i;
+}
+void quickSort(int array[], int start, int end)
+{
+    if (start < end) {
+        int pivot_index = partition(array, start, end);
+        quickSort(array, start, pivot_index - 1);
+        quickSort(array, pivot_index + 1, end);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    //int test_array[MAX_NUM] = {2, 8, 7, 1, 3, 5, 6, 4};
+    int test_array[MAX_NUM] = {13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
+    printf("Test array is:\n");
+    for (int i = 0; i < MAX_NUM; i++) {
+        printf("%d\t", test_array[i]);
+    }
+    printf("\n");
+
+    printf("Sort array is:\n");
+    quickSort(test_array, 0, MAX_NUM - 1);
+
+    for (int i = 0; i < MAX_NUM; i++) {
+        printf("%d\t", test_array[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
