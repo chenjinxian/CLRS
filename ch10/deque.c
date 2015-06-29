c7a4b4a6b69b6d391e83a3a729bb8cdeab28cb8b
chenjinxian <alanchen1203@gmail.com>
2015-06-29 22:48:47 +0800

    add deque

--- ch10/deque.c
+++ ch10/deque.c
@@ -5,89 +5,95 @@
 #define MAX_NUM 12
 #define NIL -1
 
-void printQueue(int array[], int head, int tail)
+typedef struct {
+    int array[MAX_NUM];
+    int head;
+    int tail;
+} queue;
+
+void printQueue(queue q)
 {
     for (int i = 0; i < MAX_NUM; i++) {
-        if (NIL != array[i])
-            printf("%3d\t", array[i]);
+        if (NIL != q.array[i])
+            printf("%3d\t", q.array[i]);
         else
             printf("NIL\t");
     }
     printf("\n");
-
-    if (head <= tail) {
-        for (int i = head - 1; i < tail - 1; i++)
-            printf("%3d\t", array[i]);
+    
+    if (q.head <= q.tail) {
+        for (int i = q.head - 1; i < q.tail - 1; i++)
+            printf("%3d\t", q.array[i]);
     }
     else {
-        for (int i = head - 1; i < MAX_NUM; i++)
-            printf("%3d\t", array[i]);
-        for (int i = 0; i < tail - 1; i++)
-            printf("%3d\t", array[i]);
+        for (int i = q.head - 1; i < MAX_NUM; i++)
+            printf("%3d\t", q.array[i]);
+        for (int i = 0; i < q.tail - 1; i++)
+            printf("%3d\t", q.array[i]);
     }
-
+    
     printf("\n");
 }
 
-void backEnqueue(int array[], int head, int* tail, int key)
+void backEnqueue(queue* q, int key)
 {
-    if (head == (*tail) + 1) {
+    if (q->head == q->tail + 1) {
         printf("overflow\n");
         exit(-1);
     } else {
-        array[(*tail) - 1] = key;
+        q->array[q->tail - 1] = key;
         
-        if (MAX_NUM == (*tail))
-            *tail = 1;
+        if (MAX_NUM == q->tail)
+            q->tail = 1;
         else
-            (*tail)++;
+            q->tail++;
     }
 }
 
-int frontDequeue(int array[], int* head, int tail)
+int frontDequeue(queue* q)
 {
-    if (tail == (*head)) {
+    if (q->tail == q->head) {
         printf("underflow\n");
         exit(-1);
     } else {
-        int key = array[(*head) - 1];
-
-        if (MAX_NUM == (*head))
-            *head = 1;
+        int key = q->array[q->head - 1];
+        
+        if (MAX_NUM == q->head)
+            q->head = 1;
         else
-            (*head)++;
-
+            q->head++;
+        
         return key;
     }
 }
 
-void frontEnqueue(int array[], int *head, int tail, int key)
+void frontEnqueue(queue* q, int key)
 {
-    if ((*head) == tail + 1) {
+    if (q->head == q->tail + 1) {
         printf("overflow\n");
         exit(-1);
     } else {
-        if (1 == (*head))
-            *head = MAX_NUM;
+        if (1 == q->head)
+            q->head = MAX_NUM;
         else
-            (*head)--;
+            q->head--;
         
-        array[(*head) - 1] = key;
+        q->array[q->head - 1] = key;
     }
 }
 
-int backDequeue(int array[], int head, int* tail)
+int backDequeue(queue* q)
 {
-    if (head == (*tail)) {
+    if (q->head == q->tail) {
         printf("underflow\n");
         exit(-1);
     } else {
-        if (1 == (*tail))
-            *tail = MAX_NUM;
+        if (1 == q->tail)
+            q->tail = MAX_NUM;
         else
-            (*tail)--;
+            q->tail--;
 
-        int key = array[(*tail) - 1];
+        int key = q->array[q->tail - 1];
 
         return key;
     }
@@ -97,30 +103,33 @@ int main(int argc, char* argv[])
 {
     int test_array[MAX_NUM] = {NIL, NIL, NIL, NIL, NIL, NIL, 15, 6, 9, 8, 4, NIL};
 
-    int head = 7;
-    int tail = 12;
+    queue q;
+    for (int i = 0; i < MAX_NUM; i++)
+        q.array[i] = test_array[i];
+    q.head = 7;
+    q.tail = 12;
     printf("the deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
 
-    backEnqueue(test_array, head, &tail, 17);
+    backEnqueue(&q, 17);
     printf("after back enqueue 17, deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
 
-    frontEnqueue(test_array, &head, tail, 3);
+    frontEnqueue(&q, 3);
     printf("after front enqueue 3, deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
 
-    backDequeue(test_array, head, &tail);
+    backDequeue(&q);
     printf("after back dequeue, deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
     
-    backEnqueue(test_array, head, &tail, 5);
+    backEnqueue(&q, 5);
     printf("after back enqueue 5, deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
 
-    frontDequeue(test_array, &head, tail);
+    frontDequeue(&q);
     printf("after front dequeue, deque is:\n");
-    printQueue(test_array, head, tail);
+    printQueue(q);
     
     return 0;
 }
